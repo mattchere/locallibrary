16b7564ac7ed43c3d3617573ce4b0ce0108ff7c8
infinitepure <matt.j.cheah@gmail.com>
2017-10-24 00:20:18 +1100

    Add tests for author create view

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -259,3 +259,45 @@ class RenewBookInstancesViewTest(TestCase):
         resp = self.client.post(reverse('renew-book-librarian', kwargs={'pk':self.test_bookinstance1.pk,}), {'renewal_date':invalid_date_in_future} )
         self.assertEqual( resp.status_code,200)
         self.assertFormError(resp, 'form', 'renewal_date', 'Invalid date - renewal more than 4 weeks ahead')
+
+
+class AuthorCreateViewTest(TestCase):
+
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+
+        test_user2 = User.objects.create_user(username='testuser2', password='12345')
+        test_user2.save()
+        permission = Permission.objects.get(name='Set book as returned')
+        test_user2.user_permissions.add(permission)
+        test_user2.save()
+
+    def test_redirect_if_not_logged_in(self):
+        resp = self.client.get(reverse('author_create'))
+        self.assertRedirects(resp, '/accounts/login/?next=/catalog/author/create/')
+
+    def test_redirect_if_logged_in_but_not_correct_permission(self):
+        login = self.client.login(username='testuser1', password='12345')
+        resp = self.client.get(reverse('author_create'))
+        self.assertRedirects(resp, '/accounts/login/?next=/catalog/author/create/')
+
+    def test_logged_in_with_permission(self):
+        login = self.client.login(username='testuser2', password='12345')
+        resp = self.client.get(reverse('author_create'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser2', password='12345')
+        resp = self.client.get(reverse('author_create'))
+
+        self.assertEqual(str(resp.context['user']), 'testuser2')
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'catalog/author_form.html')
+
+    def test_redirects_to_author_detail_view_on_success(self):
+        login = self.client.login(username='testuser2', password='12345')
+        resp = self.client.post(reverse('author_create'), {'first_name': 'A', 'last_name': 'B', 'date_of_birth': '03/12/1997'})
+
+        self.assertRedirects(resp, reverse('author-detail', kwargs={'pk': 1}))
